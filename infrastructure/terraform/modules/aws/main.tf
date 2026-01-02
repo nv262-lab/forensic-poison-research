d92e49279e04a734a576e33b1cf358368dbd58de
nv262-lab <nv262@cornell.edu>
2026-01-02 14:30:44 -0800

    Refactor S3 bucket resource and random_id definition

--- infrastructure/terraform/modules/aws/main.tf
+++ infrastructure/terraform/modules/aws/main.tf
@@ -1,6 +1,17 @@
-variable "prefix" { type = string }
-variable "region" { type = string }
+resource "random_id" "suffix" {
+  byte_length = 2
+}
 
-resource "random_id" "suffix" { byte_length = 4 }
-resource "aws_s3_bucket" "bucket" { bucket = "${var.prefix}-aws-bucket-${random_id.suffix.hex}" acl = "private" force_destroy = true }
-output "resources" { value = { bucket = aws_s3_bucket.bucket.id } }
+resource "aws_s3_bucket" "bucket" {
+  bucket        = "${var.prefix}-aws-bucket-${random_id.suffix.hex}"
+  acl           = "private"
+  force_destroy = true
+}
+
+variable "prefix" {
+  type = string
+}
+
+variable "region" {
+  type = string
+}
