3655dd67fd6baad23bb425c07fd938072413e736
nv262-lab <nv262@cornell.edu>
2026-01-02 14:31:23 -0800

    Refactor Azure resource group configuration

--- infrastructure/terraform/modules/azure/main.tf
+++ infrastructure/terraform/modules/azure/main.tf
@@ -1,3 +1,15 @@
-variable "prefix" { type = string }
-resource "azurerm_resource_group" "rg" { name = "${var.prefix}-rg" location = "eastus" tags = { sandbox = "true" } }
-output "resources" { value = { rg = azurerm_resource_group.rg.name } }
+resource "azurerm_resource_group" "rg" {
+  name     = "${var.prefix}-rg"
+  location = var.location
+  tags = {
+    sandbox = "true"
+  }
+}
+
+variable "prefix" {
+  type = string
+}
+
+variable "location" {
+  type = string
+}
