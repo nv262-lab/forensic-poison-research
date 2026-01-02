3d7e8821d7ce5b06d156a2153dadb7033940498f
nv262-lab <nv262@cornell.edu>
2026-01-02 14:29:56 -0800

    Add azure_location variable to variables.tf
    Added 'azure_location' variable to Terraform configuration.

--- infrastructure/terraform/variables.tf
+++ infrastructure/terraform/variables.tf
@@ -1,5 +1,29 @@
-variable "prefix" { type = string, default = "forensic-research-001-sandbox" }
-variable "aws_region" { type = string, default = "us-east-1" }
-variable "azure_subscription_id" { type = string, default = "" }
-variable "gcp_project" { type = string, default = "" }
-variable "gcp_region" { type = string, default = "us-central1" }
+variable "prefix" {
+  type    = string
+  default = "forensic-research-001-sandbox"
+}
+
+variable "aws_region" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "azure_subscription_id" {
+  type    = string
+  default = ""
+}
+
+variable "azure_location" {
+  type    = string
+  default = "eastus"
+}
+
+variable "gcp_project" {
+  type    = string
+  default = ""
+}
+
+variable "gcp_region" {
+  type    = string
+  default = "us-central1"
+}
