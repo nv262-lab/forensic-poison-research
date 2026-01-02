381d462340f28b5ac9928911ed90e6296b0a8c41
nv262-lab <nv262@cornell.edu>
2026-01-02 14:28:44 -0800

    Refactor Terraform module declarations
    Refactored module declarations for AWS, Azure, and GCP with improved formatting and added location for Azure.

--- infrastructure/terraform/main.tf
+++ infrastructure/terraform/main.tf
@@ -1,3 +1,18 @@
-module "aws" { source = "./modules/aws" prefix = var.prefix region = var.aws_region }
-module "azure" { source = "./modules/azure" prefix = var.prefix }
-module "gcp" { source = "./modules/gcp" prefix = var.prefix project = var.gcp_project region = var.gcp_region }
+module "aws" {
+  source = "./modules/aws"
+  prefix = var.prefix
+  region = var.aws_region
+}
+
+module "azure" {
+  source = "./modules/azure"
+  prefix = var.prefix
+  location = var.azure_location
+}
+
+module "gcp" {
+  source = "./modules/gcp"
+  prefix = var.prefix
+  project = var.gcp_project
+  region = var.gcp_region
+}
