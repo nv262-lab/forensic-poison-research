28c1b4a4d69665c920d4ecf99c77eb6101abf347
nv262-lab <nv262@cornell.edu>
2026-01-02 14:29:22 -0800

    Update providers.tf

--- infrastructure/terraform/providers.tf
+++ infrastructure/terraform/providers.tf
@@ -1,12 +1,30 @@
 terraform {
-  required_version = ">= 1.5.7, < 2.0.0"
   required_providers {
-    aws = { source = "hashicorp/aws", version = "4.62.0" }
-    azurerm = { source = "hashicorp/azurerm", version = "3.75.0" }
-    google = { source = "hashicorp/google", version = "4.81.0" }
-    random = { source = "hashicorp/random", version = "3.5.1" }
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 4.0"
+    }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.0"
+    }
+    google = {
+      source  = "hashicorp/google"
+      version = "~> 4.0"
+    }
   }
 }
-provider "aws" { region = var.aws_region }
-provider "azurerm" { features = {} subscription_id = var.azure_subscription_id }
-provider "google" { project = var.gcp_project region = var.gcp_region }
+
+provider "aws" {
+  region = var.aws_region
+}
+
+provider "azurerm" {
+  features = {}
+  subscription_id = var.azure_subscription_id
+}
+
+provider "google" {
+  project = var.gcp_project
+  region  = var.gcp_region
+}
