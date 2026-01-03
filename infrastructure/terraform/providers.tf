2bddf30a0a1f556ad3dd57e046a0b5d40f70b36a
nv262-lab <nv262@cornell.edu>
2026-01-02 23:31:08 -0800

    Add Azure and Google provider configurations

--- infrastructure/terraform/providers.tf
+++ infrastructure/terraform/providers.tf
@@ -23,4 +23,18 @@ terraform {
       version = ">= 3.0"
     }
   }
+
+  provider "azurerm" {
+    features {}
+    client_id       = var.azure_client_id
+    client_secret   = var.azure_client_secret
+    tenant_id       = var.azure_tenant_id
+    subscription_id  = var.azure_subscription_id
+  }
+
+  provider "google" {
+    project     = var.gcp_project
+    region      = var.gcp_region
+    credentials = var.gcp_service_account_key
+  }
 }
