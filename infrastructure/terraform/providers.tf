ad14a96a466b62e9dfcb4b798fad63c160e1fcfc
nv262-lab <nv262@cornell.edu>
2026-01-02 23:08:35 -0800

    Remove unused provider configurations
    Removed project from Google provider and subscription details from Azure provider.

--- infrastructure/terraform/providers.tf
+++ infrastructure/terraform/providers.tf
@@ -30,12 +30,9 @@ provider "aws" {
 }
 
 provider "google" {
-  project = var.gcp_project
   region  = var.gcp_region
 }
 
 provider "azurerm" {
   features {}
-  subscription_id = var.azure_subscription_id
-  tenant_id       = var.azure_tenant_id
 }
