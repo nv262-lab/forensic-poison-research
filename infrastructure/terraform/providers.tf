65af590ee3ace7ad6318510da8a9917a108ae204
nv262-lab <nv262@cornell.edu>
2026-01-02 22:10:54 -0800

    Reformat provider configurations in providers.tf

--- infrastructure/terraform/providers.tf
+++ infrastructure/terraform/providers.tf
@@ -2,11 +2,26 @@ terraform {
   required_version = ">= 1.3.0"
 
   required_providers {
-    aws     = { source = "hashicorp/aws",     version = ">= 4.0" }
-    google  = { source = "hashicorp/google",  version = ">= 4.0" }
-    azurerm = { source = "hashicorp/azurerm", version = ">= 3.0" }
-    random  = { source = "hashicorp/random",  version = ">= 3.0" }
-    null    = { source = "hashicorp/null",    version = ">= 3.0" }
+    aws     = {
+      source  = "hashicorp/aws"
+      version = ">= 4.0"
+    }
+    google  = {
+      source  = "hashicorp/google"
+      version = ">= 4.0"
+    }
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.0"
+    }
+    random  = {
+      source  = "hashicorp/random"
+      version = ">= 3.0"
+    }
+    null    = {
+      source  = "hashicorp/null"
+      version = ">= 3.0"
+    }
   }
 }
 
