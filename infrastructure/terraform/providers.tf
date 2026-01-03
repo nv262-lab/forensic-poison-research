2dd62c03e924a4cf7c20df1d77a899e4a28130ba
nv262-lab <nv262@cornell.edu>
2026-01-02 22:04:30 -0800

    Remove shared variable declarations from providers.tf
    Removed shared variables for AWS, GCP, and Azure configurations.

--- infrastructure/terraform/providers.tf
+++ infrastructure/terraform/providers.tf
@@ -10,32 +10,6 @@ terraform {
   }
 }
 
-# ----------------
-# Shared variables (declare once here)
-# ----------------
-variable "create_aws"   { type = bool, default = true }
-variable "create_gcp"   { type = bool, default = false }
-variable "create_azure" { type = bool, default = false }
-
-variable "aws_region"       { type = string, default = "us-east-1" }
-variable "tf_state_bucket"  { type = string, default = "" }    # if empty, generated
-variable "tf_lock_table"    { type = string, default = "" }    # if empty, generated
-
-variable "gcp_project" { type = string, default = "" }
-variable "gcp_region"  { type = string, default = "us-central1" }
-variable "gcs_bucket"  { type = string, default = "" }        # if empty, generated
-
-variable "azure_rg_name"         { type = string, default = "" } # if empty, generated
-variable "azure_location"        { type = string, default = "eastus" }
-variable "azure_storage_account" { type = string, default = "" } # if empty, generated
-variable "azure_container_name"  { type = string, default = "tfstate" }
-
-variable "azure_subscription_id" { type = string, default = "" }
-variable "azure_tenant_id"       { type = string, default = "" }
-
-# ----------------
-# Providers
-# ----------------
 provider "aws" {
   region = var.aws_region
 }
