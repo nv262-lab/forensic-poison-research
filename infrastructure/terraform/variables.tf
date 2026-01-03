fec62dac0fc07e6c1b9b4a5d38980cb34bc24db8
nv262-lab <nv262@cornell.edu>
2026-01-02 22:06:18 -0800

    Refactor Terraform variables for clarity and organization
    Consolidate variable definitions and remove duplicates.

--- infrastructure/terraform/variables.tf
+++ infrastructure/terraform/variables.tf
@@ -1,29 +1,21 @@
-variable "prefix" {
-  type    = string
-  default = "forensic-research-001-sandbox"
-}
+# Single place for shared variables to avoid duplicates
 
-variable "aws_region" {
-  type    = string
-  default = "us-east-1"
-}
+variable "create_aws"   { type = bool, default = true }
+variable "create_gcp"   { type = bool, default = false }
+variable "create_azure" { type = bool, default = false }
 
-variable "azure_subscription_id" {
-  type    = string
-  default = ""
-}
+variable "aws_region"       { type = string, default = "us-east-1" }
+variable "tf_state_bucket"  { type = string, default = "" }    # if empty, generated
+variable "tf_lock_table"    { type = string, default = "" }    # if empty, generated
 
-variable "azure_location" {
-  type    = string
-  default = "eastus"
-}
+variable "gcp_project" { type = string, default = "" }
+variable "gcp_region"  { type = string, default = "us-central1" }
+variable "gcs_bucket"  { type = string, default = "" }        # if empty, generated
 
-variable "gcp_project" {
-  type    = string
-  default = ""
-}
+variable "azure_rg_name"         { type = string, default = "" } # if empty, generated
+variable "azure_location"        { type = string, default = "eastus" }
+variable "azure_storage_account" { type = string, default = "" } # if empty, generated
+variable "azure_container_name"  { type = string, default = "tfstate" }
 
-variable "gcp_region" {
-  type    = string
-  default = "us-central1"
-}
+variable "azure_subscription_id" { type = string, default = "" }
+variable "azure_tenant_id"       { type = string, default = "" }
